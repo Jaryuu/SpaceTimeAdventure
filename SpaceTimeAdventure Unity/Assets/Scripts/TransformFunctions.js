746706501c2e71b0abbde8e975a89f1d90122402
ernestoivan22 <ernestoivan22@hotmail.com>
2014-04-12 13:50:41 -0600

    paquetes
    se adjuntan

---
+++ SpaceTimeAdventure Unity/Assets/Scripts/TransformFunctions.js
@@ -0,0 +1,20 @@
+﻿#pragma strict
+
+public var moveSpeed : float = 10f;
+public var turnSpeed : float = 50f;
+
+
+function Update ()
+{
+    if(Input.GetKey(KeyCode.UpArrow))
+        transform.Translate(Vector3.forward * moveSpeed * Time.deltaTime);
+    
+    if(Input.GetKey(KeyCode.DownArrow))
+        transform.Translate(-Vector3.forward * moveSpeed * Time.deltaTime);
+    
+    if(Input.GetKey(KeyCode.LeftArrow))
+        transform.Translate(Vector3.left * moveSpeed * Time.deltaTime);
+    
+    if(Input.GetKey(KeyCode.RightArrow))
+        transform.Translate(Vector3.right * moveSpeed * Time.deltaTime);
+}

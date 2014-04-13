d626585884881edc6d090035d22ba8840ff0ab15
ernestoivan22 <ernestoivan22@hotmail.com>
2014-04-12 21:41:06 -0600

    nueva version

--- SpaceTimeAdventure Unity/Assets/Scripts/ElipticalMovement.js
+++ SpaceTimeAdventure Unity/Assets/Scripts/ElipticalMovement.js
@@ -9,6 +9,7 @@
      
     function Update () {
     	X = semiMajorAxis*(1- epsilon*epsilon)/(1+ (epsilon* Mathf.Cos(2*(Mathf.PI)*time/year)))*Mathf.Cos(2*(Mathf.PI)*time/year)/100000;
-    	Z= semiMajorAxis*(1- epsilon*epsilon)/(1+ (epsilon* Mathf.Cos(2*(Mathf.PI)*time/year)))*Mathf.Sin(2*(Mathf.PI)*time/year)/100000;
+    	Z = semiMajorAxis*(1- epsilon*epsilon)/(1+ (epsilon* Mathf.Cos(2*(Mathf.PI)*time/year)))*Mathf.Sin(2*(Mathf.PI)*time/year)/100000;
     	this.gameObject.transform.position = Vector3(X,0,Z);
+    	time++;
     }
